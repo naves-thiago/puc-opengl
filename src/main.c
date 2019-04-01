75502c75fdb28478a70d5b725dbf93ba7bb961c6
Thiago Naves <naves.thiago@gmail.com>
2019-04-01 00:32:15 -0300

    Added vertex shader

--- src/main.c
+++ src/main.c
@@ -14,6 +14,47 @@ void process_input(GLFWwindow *window)
 		glfwSetWindowShouldClose(window, true);
 }
 
+float vertices[] = {
+	-0.5f, -0.5f, 0.0f,
+	 0.5f, -0.5f, 0.0f,
+	 0.0f,  0.5f, 0.0f,
+};
+
+void send_vertices()
+{
+	unsigned int VBO;
+	glGenBuffers(1, &VBO); // Create buffer ID
+	glBindBuffer(GL_ARRAY_BUFFER, VBO); // Set the ID as the active GL_ARRAY_BUFFER
+	// Copy the data. GL_STATIC_DRAW -> Data likely wont change
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+}
+
+bool compile_and_send_vertex_shader()
+{
+	static const char * const shader =
+		"#version 330 core\n"
+		"layout (location = 0) in vec3 aPos;\n"
+		"void main()\n"
+		"{\n"
+		"	gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+		"}\n";
+	unsigned int vertex_shader = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vertex_shader, 1, &shader, NULL);
+	glCompileShader(vertex_shader);
+
+	// Check for compilation errors
+	int success;
+	char info_log[512];
+	glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(vertex_shader, 521, NULL, info_log);
+		printf("ERROR: Vertex shader compilation failed.\n%s\n", info_log);
+		return false;
+	}
+	return true;
+}
+
 int main()
 {
 	glfwInit();
@@ -38,6 +79,8 @@ int main()
 	
 	glViewport(0, 0, 800, 600);
 	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+	send_vertices();
 
 	while (!glfwWindowShouldClose(window))
 	{

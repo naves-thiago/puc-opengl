e9aae81e5d9a4416eee00dd05075d3a7e3d8a769
Thiago Naves <naves.thiago@gmail.com>
2019-04-04 12:05:52 -0300

    Triangle!

--- src/main.c
+++ src/main.c
@@ -2,6 +2,7 @@
 #include <GLFW/glfw3.h>
 #include <stdio.h>
 #include <stdbool.h>
+#include <stdlib.h>
 
 void framebuffer_size_callback(GLFWwindow *window, int width, int height)
 {
@@ -15,18 +16,55 @@ void process_input(GLFWwindow *window)
 }
 
 float vertices[] = {
-	-0.5f, -0.5f, 0.0f,
-	 0.5f, -0.5f, 0.0f,
-	 0.0f,  0.5f, 0.0f,
+	-0.5f, -0.5f, 0.0f, // left
+	 0.5f, -0.5f, 0.0f, // right
+	 0.0f,  0.5f, 0.0f, // top
 };
 
+unsigned int VBO, VAO;
 void send_vertices()
 {
-	unsigned int VBO;
 	glGenBuffers(1, &VBO); // Create buffer ID
-	glBindBuffer(GL_ARRAY_BUFFER, VBO); // Set the ID as the active GL_ARRAY_BUFFER
+
+	// Create VAO (Vertex Array Object)
+	glGenVertexArrays(1, &VAO);
+	glBindVertexArray(VAO);
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
 	// Copy the data. GL_STATIC_DRAW -> Data likely wont change
 	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// Uses the last buffer associated with GL_ARRAY_BUFFER
+	// Params: attribute location, attribute size, type, normalize, stride, start position (in the buffer)
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void *)0);
+	glEnableVertexAttribArray(0); // param: attribute location
+}
+
+bool check_shader_compilation(unsigned int shader, char *name)
+{
+	// Check for compilation errors
+	int success;
+	char info_log[512];
+	glGetShaderiv(shader, GL_COMPILE_STATUS, &success);
+	if (!success)
+	{
+		glGetShaderInfoLog(shader, 521, NULL, info_log);
+		printf("ERROR: %s compilation failed.\n%s\n", name, info_log);
+	}
+	return success;
+}
+
+bool check_program_link(unsigned int program, char *name)
+{
+	// Check for compilation errors
+	int success;
+	char info_log[512];
+	glGetProgramiv(program, GL_LINK_STATUS, &success);
+	if (!success)
+	{
+		glGetProgramInfoLog(program, 521, NULL, info_log);
+		printf("ERROR: %s link failed.\n%s\n", name, info_log);
+	}
+	return success;
 }
 
 bool compile_and_send_vertex_shader()
@@ -41,18 +79,26 @@ bool compile_and_send_vertex_shader()
 	unsigned int vertex_shader = glCreateShader(GL_VERTEX_SHADER);
 	glShaderSource(vertex_shader, 1, &shader, NULL);
 	glCompileShader(vertex_shader);
+	if (!check_shader_compilation(vertex_shader, "Vertex shader"))
+		exit(-1);
+	return vertex_shader;
+}
 
-	// Check for compilation errors
-	int success;
-	char info_log[512];
-	glGetShaderiv(vertex_shader, GL_COMPILE_STATUS, &success);
-	if (!success)
-	{
-		glGetShaderInfoLog(vertex_shader, 521, NULL, info_log);
-		printf("ERROR: Vertex shader compilation failed.\n%s\n", info_log);
-		return false;
-	}
-	return true;
+bool compile_and_send_fragment_shader()
+{
+	static const char * const shader =
+		"#version 330 core\n"
+		"out vec4 FragColor;\n"
+		"void main()\n"
+		"{\n"
+		"	FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+		"}\n";
+	unsigned int frag_shader = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(frag_shader, 1, &shader, NULL);
+	glCompileShader(frag_shader);
+	if (!check_shader_compilation(frag_shader, "Fragment shader"))
+		exit(-1);
+	return frag_shader;
 }
 
 int main()
@@ -82,6 +128,19 @@ int main()
 
 	send_vertices();
 
+	// Create shaders
+	unsigned int vertex_shader = compile_and_send_vertex_shader();
+	unsigned int frag_shader = compile_and_send_fragment_shader();
+	unsigned int shader_program = glCreateProgram();
+	glAttachShader(shader_program, vertex_shader);
+	glAttachShader(shader_program, frag_shader);
+	glLinkProgram(shader_program);
+	if (!check_program_link(shader_program, "Shader program"))
+		exit(-1);
+	
+	glDeleteShader(vertex_shader);
+	glDeleteShader(frag_shader);
+
 	while (!glfwWindowShouldClose(window))
 	{
 		process_input(window);
@@ -90,9 +149,21 @@ int main()
 		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
 		glClear(GL_COLOR_BUFFER_BIT);
 
+		// Draw triangle
+		glUseProgram(shader_program);
+		glBindVertexArray(VAO); // No need to bind every time, since we only got 1, but
+		// Params: mode, start index, vertex count
+		glDrawArrays(GL_TRIANGLES, 0, 3);
+		//glBindVertexArray(0); // No need to unbind every time
+
 		glfwSwapBuffers(window);
 		glfwPollEvents();
 	}
+
+	// de-allocate resources
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+
 	glfwTerminate();
 	return 0;
 }

8416249792ef07a52f43da1947ef0b220e06e978
Thiago Naves <naves.thiago@gmail.com>
2019-04-23 22:20:47 -0300

    Update Texture2D class to support alpha channel in files

--- src/texture.cc
+++ src/texture.cc
@@ -16,7 +16,8 @@ Texture2D::Texture2D(const std::string &path, unsigned int location,
 	stbi_set_flip_vertically_on_load(verticalFlip);
 	unsigned char *data = stbi_load(path.c_str(), &width, &height, &nrChannels, 0);
 	if (data) {
-		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB,
+		glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0,
+				nrChannels == 4 ? GL_RGBA : GL_RGB,
 				GL_UNSIGNED_BYTE, data);
 		glGenerateMipmap(GL_TEXTURE_2D);
 	}

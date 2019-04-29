20dcecd2109666cda0c289ddcb3cf027e9c61966
Thiago Naves <naves.thiago@gmail.com>
2019-04-29 00:50:33 -0300

    Added camera class

---
+++ 06.3-camera-walk-camera-class/src/main.cc
@@ -0,0 +1,213 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+#include <shader.hh>
+#include <texture.hh>
+#include <camera.hh>
+#include <iostream>
+
+void framebuffer_size_callback(GLFWwindow* window, int width, int height);
+void mouse_callback(GLFWwindow* window, double xpos, double ypos);
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void processInput(GLFWwindow *window);
+
+const unsigned int SCR_WIDTH = 800;
+const unsigned int SCR_HEIGHT = 600;
+
+Camera camera((float)SCR_WIDTH / SCR_HEIGHT);
+
+int main()
+{
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+#ifdef __APPLE__
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // uncomment this statement to fix compilation on OS X
+#endif
+
+	GLFWwindow* window = glfwCreateWindow(SCR_WIDTH, SCR_HEIGHT, "LearnOpenGL", NULL, NULL);
+	if (window == NULL)
+	{
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+	glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+	glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED); // Hide / capture cursor
+	glfwSetCursorPosCallback(window, mouse_callback);
+	glfwSetScrollCallback(window, scroll_callback);
+
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
+	{
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	Shader ourShader("shader.vs", "shader.fs");
+
+	float vertices[] = {
+		// positions          // texture coords
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 0.0f,
+
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 1.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f, -0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f, -0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f, -0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f, -0.5f, -0.5f,  0.0f, 1.0f,
+
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f,
+		 0.5f,  0.5f, -0.5f,  1.0f, 1.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		 0.5f,  0.5f,  0.5f,  1.0f, 0.0f,
+		-0.5f,  0.5f,  0.5f,  0.0f, 0.0f,
+		-0.5f,  0.5f, -0.5f,  0.0f, 1.0f
+	};
+
+	glEnable(GL_DEPTH_TEST);
+
+	unsigned int VBO, VAO;
+	glGenVertexArrays(1, &VAO);
+	glGenBuffers(1, &VBO);
+	// bind the Vertex Array Object first, then bind and set vertex buffer(s), and then configure vertex attributes(s).
+	glBindVertexArray(VAO);
+
+	glBindBuffer(GL_ARRAY_BUFFER, VBO);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+	// position attribute
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
+	glEnableVertexAttribArray(0);
+	// texture attribute
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)(3 * sizeof(float)));
+	glEnableVertexAttribArray(1);
+
+	Texture2D tex1("container.jpg", 0);
+	Texture2D tex2("awesomeface.png", 1);
+
+	ourShader.use();
+	ourShader.setInt("texture1", 0);
+	ourShader.setInt("texture2", 1);
+	tex1.activateAndBind();
+	tex2.activateAndBind();
+
+	// You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+	// VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+	// glBindVertexArray(0);
+
+	glm::vec3 cubePositions[] = {
+		glm::vec3( 0.0f,  0.0f,  0.0f),
+		glm::vec3( 2.0f,  5.0f, -15.0f),
+		glm::vec3(-1.5f, -2.2f, -2.5f),
+		glm::vec3(-3.8f, -2.0f, -12.3f),
+		glm::vec3( 2.4f, -0.4f, -3.5f),
+		glm::vec3(-1.7f,  3.0f, -7.5f),
+		glm::vec3( 1.3f, -2.0f, -2.5f),
+		glm::vec3( 1.5f,  2.0f, -2.5f),
+		glm::vec3( 1.5f,  0.2f, -1.5f),
+		glm::vec3(-1.3f,  1.0f, -1.5f)
+	};
+
+	while (!glfwWindowShouldClose(window))
+	{
+		processInput(window);
+
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		for (int i=0; i<10; i++)
+		{
+			glm::mat4 model = glm::mat4(1.0f);
+			model = glm::translate(model, cubePositions[i]);
+			float angle = 20.0f * i;
+			model = glm::rotate(model, glm::radians(angle), glm::vec3(1.0f, 0.3f, 0.5f));
+			ourShader.setMat("model", model);
+
+			glDrawArrays(GL_TRIANGLES, 0, 36);
+		}
+
+		glm::mat4 model = glm::mat4(1.0f);
+		model = glm::translate(model, glm::vec3(1, -0.6, 0));
+		model = glm::rotate(model, (float)glfwGetTime() * glm::radians(100.0f), glm::vec3(0.5f, 1.0f, 0.0f));
+		model = glm::scale(model, glm::vec3(0.4f, 0.4f, 0.4f));
+		ourShader.setMat("model", model);
+
+		// Camera
+		ourShader.setMat("view", camera.view_matrix());
+		ourShader.setMat("projection", camera.projection_matrix());
+
+		// render the scene
+		ourShader.use();
+		glBindVertexArray(VAO);
+		glDrawArrays(GL_TRIANGLES, 0, 36);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glDeleteVertexArrays(1, &VAO);
+	glDeleteBuffers(1, &VBO);
+	glfwTerminate();
+	return 0;
+}
+
+// process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly
+// ---------------------------------------------------------------------------------------------------------
+void processInput(GLFWwindow *window)
+{
+	if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, true);
+
+	camera.key_press(window);
+}
+
+// glfw: whenever the window size changed (by OS or user resize) this callback function executes
+// ---------------------------------------------------------------------------------------------
+void framebuffer_size_callback(GLFWwindow* window, int width, int height)
+{
+	// make sure the viewport matches the new window dimensions; note that width and 
+	// height will be significantly larger than specified on retina displays.
+	glViewport(0, 0, width, height);
+	camera.set_aspect_ratio((float)width / height);
+}
+
+void mouse_callback(GLFWwindow* window, double xpos, double ypos)
+{
+	camera.mouse_move(xpos, ypos);
+}
+
+void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
+{
+	camera.zoom(yoffset);
+}
---
+++ inc/camera.hh
@@ -0,0 +1,255 @@
+#ifndef CAMERA_HH
+#define CAMERA_HH
+
+#include <glad/glad.h>
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <GLFW/glfw3.h>
+
+enum Direction {
+	FORWARD,
+	BACKWARD,
+	LEFT,
+	RIGHT,
+};
+
+class GenericCamera {
+	float pitch;
+	float yaw;
+	float fov;
+	float max_fov;
+	float min_fov;
+	float move_speed;
+	float look_sensitivity;
+	float zoom_sensitivity;
+	float aspect_ratio;
+	glm::vec3 position;
+	glm::vec3 front;
+	glm::vec3 up;
+	glm::vec3 right;
+	glm::vec3 world_up;
+
+	void update_vectors(void) {
+		glm::vec3 new_front;
+		new_front.x = cos(glm::radians(pitch)) * cos(glm::radians(yaw));
+		new_front.y = sin(glm::radians(pitch));
+		new_front.z = cos(glm::radians(pitch)) * sin(glm::radians(yaw));
+		front = glm::normalize(new_front);
+		// Also re-calculate the right and up vectors
+		// Normalize the vectors, because their length gets closer to 0 the
+		// more you look up or down which results in slower movement.
+		//right = glm::normalize(glm::cross(front, up));
+		right = glm::normalize(glm::cross(front, world_up));
+		up    = glm::normalize(glm::cross(right, front));
+	}
+
+	void _set_pitch(float value) {
+		if (value < -89.0f)
+			pitch = -89.0f;
+		else if (value > 89.0f)
+			pitch = 89.0f;
+		else
+			pitch = value;
+	}
+
+	void _set_yaw(float value) {
+		yaw = value;
+	}
+
+public:
+	GenericCamera(float aspect_ratio) : aspect_ratio(aspect_ratio) {
+		pitch   = 0.0f;
+		yaw     = -90.0f;
+		fov     = 45.0f;
+		max_fov = 45.0f;
+		min_fov = 1.0f;
+		move_speed       = 4.0f;
+		look_sensitivity = 0.05f;
+		zoom_sensitivity = 1.0f;
+		position = glm::vec3(0.0f, 0.0f,  3.0f);
+		front    = glm::vec3(0.0f, 0.0f, -1.0f);
+		up       = glm::vec3(0.0f, 1.0f,  0.0f);
+		right    = glm::vec3(1.0f, 0.0f,  0.0f);
+		world_up = glm::vec3(0.0f, 1.0f,  0.0f);
+	}
+
+	glm::mat4 view_matrix(void) {
+		return glm::lookAt(position, position + front, up);
+	}
+
+	glm::mat4 projection_matrix(void) {
+		return glm::perspective(glm::radians(fov), aspect_ratio, 0.1f, 100.0f);
+	}
+
+	void look(float xoffset, float yoffset) {
+		xoffset = xoffset * look_sensitivity;
+		yoffset = yoffset * look_sensitivity;
+		_set_yaw(yaw + xoffset);
+		_set_pitch(pitch + yoffset);
+		update_vectors();
+	}
+
+	void move(Direction dir, float delta_time) {
+		float offset = move_speed * delta_time;
+		switch (dir) {
+		case FORWARD:  position += front * offset; break;
+		case BACKWARD: position -= front * offset; break;
+		case LEFT: position  -= right * offset; break;
+		case RIGHT: position += right * offset; break;
+		}
+	}
+
+	void zoom(float offset) {
+		set_fov(fov - offset * zoom_sensitivity);
+	}
+
+	void set_aspect_ratio(float value) {
+		aspect_ratio = value;
+	}
+
+	void set_fov_limits(float fmin, float fmax) {
+		min_fov = fmin;
+		max_fov = fmax;
+	}
+
+	void set_fov(float value) {
+		if (value < min_fov)
+			fov = min_fov;
+		else if (value > max_fov)
+			fov = max_fov;
+		else
+			fov = value;
+	}
+
+	void set_pitch(float value) {
+		_set_pitch(value);
+		update_vectors();
+	}
+
+	void set_yaw(float value) {
+		_set_yaw(value);
+		update_vectors();
+	}
+
+	void set_move_seed(float value) {
+		move_speed = value;
+	}
+
+	void set_look_sensitivity(float value) {
+		look_sensitivity = value;
+	}
+
+	void set_zoom_sensitivity(float value) {
+		zoom_sensitivity = value;
+	}
+
+	void set_pos(glm::vec3 value) {
+		position = value;
+	}
+
+	void set_pos(float x, float y, float z) {
+		set_pos(glm::vec3(x, y, z));
+	}
+
+	void set_pos(glm::vec3 pos, float pitch, float yaw, float fov) {
+		this->position = pos;
+		_set_pitch(pitch);
+		_set_yaw(yaw);
+		set_fov(fov);
+		update_vectors();
+	}
+
+	void set_pos(float x, float y, float z, float pitch, float yaw, float fov) {
+		set_pos(glm::vec3(x, y, z), pitch, yaw, fov);
+	}
+
+	void set_front(glm::vec3 value) {
+		front = value;
+	}
+
+	void set_front(float x, float y, float z) {
+		set_front(glm::vec3(x, y, z));
+	}
+
+	void set_up(glm::vec3 value) {
+		up = value;
+	}
+
+	void set_up(float x, float y, float z) {
+		up = glm::vec3(x, y, z);
+	}
+};
+
+class Camera : public GenericCamera {
+	bool first_mouse;     // First mouse position call
+	float last_x, last_y; // Last mouse position
+	float delta_time;     // Time between current frame and last frame
+	float last_frame;     // Time of the last frame
+	glm::vec3 default_pos;
+	float default_pitch;
+	float default_yaw;
+	float default_fov;
+
+public:
+	Camera(float aspect_ratio) : GenericCamera(aspect_ratio) {
+		first_mouse = true;
+		delta_time = 0.0f;
+		last_frame = 0.0f;
+		default_pos = glm::vec3(0.0f, 0.0f, 3.0f);
+		default_pitch = 0.0f;
+		default_yaw = -90.0f;
+		default_fov = 45.0f;
+	}
+
+
+	void set_default_pos(glm::vec3 pos, float pitch, float yaw, float fov) {
+		default_pos = pos;
+		default_pitch = pitch;
+		default_yaw = yaw;
+		default_fov = fov;
+	}
+
+	void set_default_pos(float x, float y, float z, float pitch, float yaw, float fov) {
+		set_default_pos(glm::vec3(x, y, z), pitch, yaw, fov);
+	}
+
+	void mouse_move(float xpos, float ypos) {
+		if (first_mouse) {
+			last_x = xpos;
+			last_y = ypos;
+			first_mouse = false;
+			return;
+		}
+
+		float xoffset = xpos - last_x;
+		float yoffset = last_y - ypos; // reversed y
+		last_x = xpos;
+		last_y = ypos;
+		look(xoffset, yoffset);
+	}
+
+	void key_press(GLFWwindow *window)
+	{
+		float current_frame = glfwGetTime();
+		delta_time = current_frame - last_frame;
+		last_frame = current_frame;
+
+		if (glfwGetKey(window, GLFW_KEY_W) == GLFW_PRESS)
+			move(FORWARD, delta_time);
+
+		if (glfwGetKey(window, GLFW_KEY_S) == GLFW_PRESS)
+			move(BACKWARD, delta_time);
+
+		if (glfwGetKey(window, GLFW_KEY_A) == GLFW_PRESS)
+			move(LEFT, delta_time);
+
+		if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
+			move(RIGHT, delta_time);
+
+		if (glfwGetKey(window, GLFW_KEY_SPACE) == GLFW_PRESS) {
+			set_pos(default_pos, default_pitch, default_yaw, default_fov);
+		}
+	}
+};
+
+#endif

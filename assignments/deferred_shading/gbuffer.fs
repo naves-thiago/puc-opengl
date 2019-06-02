9f4d64eb9bf0b6f05a01215d133e3c7064b09def
Thiago Naves <naves.thiago@gmail.com>
2019-06-02 17:11:21 -0300

    Light calculation in the eye space

--- assignments/deferred_shading/gbuffer.fs
+++ assignments/deferred_shading/gbuffer.fs
@@ -1,38 +1,41 @@
 #version 330 core
 
 struct Light {
-	vec3 position; // Not used in the fragment
+	vec3 position; // Light position (world space)
 	vec3 ambient;  // Ambient color
 	vec3 diffuse;  // Diffuse color
 	vec3 specular; // Specular color
 };
 
-in vec2 TexCoords;       // Texture coords
-in vec3 TangentLightPos; // Light Position (tangent space)
-in vec3 TangentFragPos;  // Fragment Position (tangent space)
+in vec3 FragPos;   // Fragment position (eye space)
+in vec2 TexCoords; // Texture coords
+in mat3 TBN;
 
 uniform float shininess;
 uniform Light light;
 uniform sampler2D normalMap;
+uniform mat4 view;
 
 out vec4 FragColor;
 
 void main() {
 	vec3 normal = texture(normalMap, TexCoords).rgb;
 	normal = normalize(normal * 2.0 - 1.0);
+	normal = normalize(TBN * normal);
 
 	vec3 color = vec3(1.0);
 
 	vec3 ambient = light.ambient * color;
 
-	vec3 lightDir = normalize(TangentLightPos - TangentFragPos);
+	vec3 lightPos = vec3(view * vec4(light.position, 1.0));
+	vec3 lightDir = normalize(lightPos - FragPos);
 	float diff = max(dot(normal, lightDir), 0.0);
 	vec3 diffuse = diff * light.diffuse * color;
 
-	vec3 viewDir = normalize(-TangentFragPos);
+	vec3 viewDir = normalize(-FragPos);
 	vec3 reflectDir = reflect(-lightDir, normal);
 	float spec = pow(max(dot(viewDir, reflectDir), 0.0), shininess);
-	vec3 specular = spec * light.specular;;
+	vec3 specular = spec * light.specular;
 
 	vec3 result = ambient + diffuse + specular;
 	FragColor = vec4(result, 1.0);

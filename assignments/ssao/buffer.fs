69da5598033a55cd4405af27106762d1692e69dc
Thiago Naves <naves.thiago@gmail.com>
2019-07-05 23:38:17 -0300

    Experimenting with how to show the position buffer

--- assignments/ssao/buffer.fs
+++ assignments/ssao/buffer.fs
@@ -7,5 +7,13 @@ uniform sampler2D gBuffer;
 
 void main() {
 	vec3 color = texture(gBuffer, TexCoords).rgb;
+	//vec3 color = normalize(abs(texture(gBuffer, TexCoords).rgb));
+	//vec3 color = texture(gBuffer, TexCoords).rgb;
+	//float offset = 0;
+	//offset = min(offset, color.r);
+	//offset = min(offset, color.g);
+	//offset = min(offset, color.b);
+	//color = normalize(color - vec3(1.0) * offset);
+
 	FragColor = vec4(color, 1.0);
 }
